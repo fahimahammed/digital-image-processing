17153cc4e2359173886e1bf20d8ed64222003085
fahimahammed <fahimahammed.cse@gmail.com>
2023-09-09 11:26:52 +0600

    image rgb

--- rgb-to-gray-c.py
+++ rgb-to-gray-c.py
@@ -3,6 +3,9 @@ import numpy as np
 
 image = cv2.imread("photos/photo-5.webp")
 grayImage = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+# image[:, :, 0]=0 // blue = 0
+
 print(image.shape)
 print(grayImage.shape)
 
