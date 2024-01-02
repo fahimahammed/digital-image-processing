3dd1e45bf2d251720040be044615c586ed8a9db7
fahimahammed <fahimahammed.cse@gmail.com>
2024-01-02 18:34:53 +0600

    decrease-spatial-resolution

--- practice/test.py
+++ practice/test.py
@@ -1,7 +1,13 @@
 import cv2
+import matplotlib.pyplot as plt
 
-print("OpenCV version:", cv2.__version__)
-
+# Load the grayscale image with error handling
+try:
+    image = cv2.imread("photos/photo-5.webp", 0)
+    print("Image loaded: ", image)
+except cv2.error as e:
+    print("Error loading image:", e)
+    exit()  # Or take alternative actions
 
 
 
